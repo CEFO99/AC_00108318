de5016a378ef9525c6df060158e6984563a49459
CEFO99 <cesarefo21@gmail.com>
2021-04-19 20:01:36 -0600

    Laboratorio 2

--- Tarea Laboratorio 2/Labo2_00108318.asm
+++ Tarea Laboratorio 2/Labo2_00108318.asm
@@ -1,21 +1,21 @@
         org 100h
         section .text
 
-        mov	AL, "C"
-	    mov	CL, "E"
-	    mov	DH, "F"
+        mov AL, "C"
+	    mov CL, "E"
+	    mov DH, "F"
         mov DL, "O"
 
-        mov	[200h], AL
-	    mov	[201h], CL
-	    mov	[202h], DH
+        mov [200h], AL
+        mov [201h], CL
+        mov [202h], DH
         mov [203h], DL
 
         mov SI, 2h
         mov BX, 200h
 
     ; Direccionamiento directo o absoluto 
-	    mov 	AX, [200h]
+	    mov AX, [200h]
 
     ; Direccionamiento indirecto por registro
     ; Usamos el registro base como puntero para acceder a la memoria
@@ -23,7 +23,7 @@
 
     ; Direccionamiento indirecto base mas indice 
     ; Usamos el registro base junto con un registro apuntador
-	    mov 	DX, [BX+SI]
+	    mov DX, [BX+SI]
     
     ; Direccionamiento relativo por registro
 	    mov	DI, [BX+3h]
